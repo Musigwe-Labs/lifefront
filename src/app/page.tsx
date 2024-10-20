80093bb599bbe770286a1342b67b6e395303c2c5
Ernest2026 <chidiebereernest8@gmail.com>
2024-10-20 12:33:52 +0100

    random commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { redirect, useSearchParams } from 'next/navigation'
+import { redirect, useSearchParams,  } from 'next/navigation'
 
 export default function Profile() {
     const searchParams = useSearchParams()
